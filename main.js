bafb13b3a03ff158bace984254413fdc0c8a5cc5
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-05-18 19:32:07 -0300

    final da aula 01

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+
+/*    -------------- DATA ---------
+var d = new Date();
+alert(d);
+*/
+
+/*    --------  REPETIÇÃO COM FOR -------
+var count;
+for (count=1; count<=5;count++){
+    alert(count);
+};
+*/
+
+/* ------ REPETIÇÃO COM WHILE--------
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++; 
+}
+*/
+
+
+
+/*     ENTRADA DE DADOS E CONDICIONAL
+var idade = prompt("Olá, qual é a sua idade?");
+if (idade >= 18) {
+    alert('Ok, você é maior de idade!');
+}else{ alert('Você é menor de idade!')
+};
+*/
+
+
+/*      ----------- AULA 02 - ARRAY/LISTA
+var lista = ['maçã','pêra', 'banana'];//lista ou array
+console.log(lista);// imprimindo a lista no console
+alert(lista);//imprimindo a lista no alerta
+console.log(lista[2]);//escolhendo item da lista
+alert(lista[0]);//escolhendo item da lista
+lista.push('abacaxi');//incluindo item na lista
+alert(lista);
+lista.pop();//tira o ultimo item da lista
+alert(lista);
+*/
+
+/*       ----AULA 01- INTRODUÇÃO AO JS-----
 var nome = "Rômulo Goleniesky"
 alert("Vamos começar! " +nome);
 console.log(nome);
@@ -5,3 +51,4 @@ console.log(nome);
 var frase = "Meu nome é Aluno"
 console.log(frase);
 console.log(frase.replace("Aluno","Rômulo"));
+*/
