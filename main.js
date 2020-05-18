24bf19f186fcd9c9f16c11289a6676ef4140ac2b
romulogoleniesky <62076782+romulogoleniesky@users.noreply.github.com>
2020-05-18 17:45:11 -0300

    primeiros comandos com JavaScript

---
+++ main.js
@@ -0,0 +1,7 @@
+var nome = "Rômulo Goleniesky"
+alert("Vamos começar! " +nome);
+console.log(nome);
+// TESTE COMO TROCAR UM DADO DE UMA STRING:
+var frase = "Meu nome é Aluno"
+console.log(frase);
+console.log(frase.replace("Aluno","Rômulo"));
