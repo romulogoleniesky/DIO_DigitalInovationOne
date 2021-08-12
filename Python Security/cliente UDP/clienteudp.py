8034115164d5af9cb3261bfcb9b63ba605815e4b
RomuloGoleniesky <goleniesky@hotmail.com>
2021-08-11 22:27:45 -0300

    Criado o clienteudp.py

---
+++ Python Security/cliente UDP/clienteudp.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print('Conexão estabelecida!')
+host = "localhost"
+porta = 5433
+mensagem = "Servidor OK!"
+
+try:
+    print('Cliente:'+ mensagem)
+    s.sendto(mensagem.encode(), (host,5432))
+
+    dados, servidor = s.recvfrom(4096)
+    dados = dados.decode()
+    print('Cliente'+ dados)
+finally:
+    print("processo ok")
+    s.close()
