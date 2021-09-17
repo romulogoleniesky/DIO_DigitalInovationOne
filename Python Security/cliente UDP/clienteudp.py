1a5af86650e137a0cbb71ab5cd777600769877f7
RomuloGoleniesky <goleniesky@hotmail.com>
2021-09-17 08:25:54 -0300

    cliente udp

--- Python Security/cliente UDP/clienteudp.py
+++ Python Security/cliente UDP/clienteudp.py
@@ -7,12 +7,12 @@ porta = 5433
 mensagem = "Servidor OK!"
 
 try:
-    print('Cliente:'+ mensagem)
-    s.sendto(mensagem.encode(), (host,5432))
+    print('Cliente:' + mensagem)
+    s.sendto(mensagem.encode(), (host, 5432))
 
     dados, servidor = s.recvfrom(4096)
     dados = dados.decode()
-    print('Cliente'+ dados)
+    print('Cliente' + dados)
 finally:
     print("processo ok")
     s.close()
